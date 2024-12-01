274d45f072510e05b5b1b4575fdda41df847dbed
Muhammad Mahad Zeeshan <u2023462@giki.edu.pk>
2024-12-01 17:14:26 +0500

    Added "Edge Addition" and displaying functions.

--- Graph_datastructure_C++.cpp
+++ Graph_datastructure_C++.cpp
@@ -20,4 +20,22 @@ class graph{
             delete []AdjacencyMatrix[i];
         }
         delete []AdjacencyMatrix;
-    }};
+    }
+        void addEgde(int v,int u){
+            if(v>=vertices||u>=vertices||v<0||u<0){
+                cout<<"Invalid edge.."<<endl;
+            }
+            else{
+                AdjacencyMatrix[v][u]=1;
+                AdjacencyMatrix[u][v]=1;//For undirected graphs...
+            }
+        }
+    void display_graph(){
+        for(int i=0;i<vertices;i++){
+            for(int j=0;j<vertices;j++){
+                cout<<AdjacencyMatrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }    
+};
