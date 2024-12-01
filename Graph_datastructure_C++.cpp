6f80a8285146946cde75eab9ae7d3d088065afbc
Muhammad Mahad Zeeshan <u2023462@giki.edu.pk>
2024-12-01 17:17:37 +0500

    Added Breadth First Search(BFS) function.

--- Graph_datastructure_C++.cpp
+++ Graph_datastructure_C++.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<queue>//For storing record of visited vertices
 using namespace std;
 
 class graph{
@@ -30,12 +30,32 @@ class graph{
                 AdjacencyMatrix[u][v]=1;//For undirected graphs...
             }
         }
-    void display_graph(){
-        for(int i=0;i<vertices;i++){
-            for(int j=0;j<vertices;j++){
+        void display_graph(){
+            for(int i=0;i<vertices;i++){
+                for(int j=0;j<vertices;j++){
                 cout<<AdjacencyMatrix[i][j]<<" ";
             }
             cout<<endl;
         }
+    }
+        void Breadth_first_search(int start){
+        bool *visited=new bool[vertices]();
+        queue<int>q;
+        cout<<"Breadth first search..."<<endl;
+        visited[start]=true;
+        q.push(start);
+        while(!q.empty()){
+            int current=q.front();
+            q.pop();
+            cout<<current<<" ";
+            for(int i=0;i<vertices;i++){
+                if(AdjacencyMatrix[current][i]==1&&!visited[i]){
+                    visited[i]=true;
+                    q.push(i);
+                }
+            }
+        }
+        cout<<endl;
+        delete []visited;
     }    
 };
